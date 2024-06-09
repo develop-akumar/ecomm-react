336851724ce6f14dbe84a5f774a35d12e52a9864
develop-akumar <develop.akumar@gmail.com>
2024-06-10 00:02:18 +0530

    insert table in product list component

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
 import Header from './Header'
+import Table from 'react-bootstrap/Table';
 
 export default function ProductList() {
   return (
     <div>
         <Header/>
         <h1>ProductList</h1>
+        <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Image Name</th>
+          <th>Price</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Larry the Bird</td>
+          <td>@twitter</td>
+          <td>@fat</td>
+        </tr>
+      </tbody>
+    </Table>
 
     </div>
   )
