3006a82c2095c24ef5d3bdd126fe22b7e451ec74
develop-akumar <develop.akumar@gmail.com>
2024-06-09 23:22:42 +0530

    Add product form

--- src/Components/AddProduct.jsx
+++ src/Components/AddProduct.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from './Header'
+import { Button } from 'react-bootstrap'
 
 export default function AddProduct() {
+
+  const [name, setName] = useState("")
+  const [file, setFile] = useState("")
+  const [price, setPrice] = useState("")
+  const [desc, setDesc] = useState("")
+
+  function addProduct(){
+    console.log('info = ', {name, file, price, desc});
+
+    let formData = new FormData()
+    formData.append("name", name)
+    formData.append("file", file)
+    formData.append("price", price)
+    formData.append("desc", desc)
+
+    
+  }
   return (
-    <div>
+    <>
       <Header />
+    <div className='col-sm-6 offset-sm-3'>
       <h1>AddProduct</h1>
+      <input type='text' onChange={(e)=>{setName(e.target.value)}} className='form-control' placeholder='Enter Name' />
+      <br />
+      <input type='file' onChange={(e)=>{setFile(e.target.files[0])}} className='form-control' placeholder='Select file' />
+      <br />
+      <input type='text' onChange={(e)=>{setPrice(e.target.value)}} className='form-control' placeholder='Enter Price' />
+      <br />
+      <input type='text' onChange={(e)=>{setDesc(e.target.value)}} className='form-control' placeholder='Enter Description' />
+      <br />
+      <Button onClick={addProduct}>Add Product</Button>
     </div>
+    </>
+    
   )
 }
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,10 +1,35 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import Header from './Header'
 import { useNavigate } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
 export default function Login() {
   const navigate = useNavigate()
-  
+
+  const [email, setEmail] = useState("anil@test.com")
+  const [pwd, setPwd] = useState("abc")
+
+  async function login(){
+    let item = {email, pwd}
+    let url = "https://jsonplaceholder.typicode.com/posts"
+    let result = fetch(url, {
+      method:"POST", 
+      body:JSON.stringify(item), 
+      headers:{
+        "Content-Type" : "application/json; charset=UTF-8",
+        "Accept" : "application/json"
+      } 
+    }).then((res)=>{
+      console.log('res = ', res);
+      return res.json()})
+    .then((json)=>{
+      let resData = JSON.stringify(json)
+      console.log('JSON.parse(JSON.stringify(json)) = ', resData);
+      localStorage.setItem("user-info", resData)
+      navigate("/add")
+    })
+  }
+
   useEffect((ele)=>{
     if(localStorage.getItem("user-info")){
       navigate("/add")
@@ -13,7 +38,14 @@ export default function Login() {
   return (
     <>
     <Header/>
-    <div>Login</div>
+    <div className='col-sm-6 offset-sm-3'>
+    <h1>Login</h1>
+     <input type='text' className='form-control' placeholder='Enter Email'/>
+     <br />
+     <input type='password' className='form-control' placeholder='Enter Password' />
+     <br />
+     <Button onClick={login}>Login</Button>
+     </div>
     </>
   )
 }
